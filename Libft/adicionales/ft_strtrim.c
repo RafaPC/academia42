acf3c0638d34835c9174fa2e8af3dd13c3d3eaf6
RafaPC <rafitap.c@hotmail.com>
2019-11-18 20:33:04 +0100

    ft_strtrim arreglado

--- Libft/adicionales/ft_strtrim.c
+++ Libft/adicionales/ft_strtrim.c
@@ -18,27 +18,29 @@ int		ift_checkchar(char c, char *set);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	unsigned int start;
-	unsigned int end;
+	int start;
+	int end;
 	unsigned int i;
 	char *new_string;
 
-	start = get_limit(s1, set, -1);
-	end = get_limit(s1, set, 1);
-	new_string = (char*)malloc((end - start) * sizeof(char));
+	start = get_limit(s1, set, 1);
+	end = get_limit(s1, set, -1) + 1;
+	printf("%d-%d", start, end);
+	new_string = (char*)malloc(((end - start) + 1) * sizeof(char));
 	i = 0;
 	while ((i + start) < end)
 	{
 		new_string[i] = s1[i + start];
 		i++;
 	}
+	new_string[i] = '\0';
 	return (new_string);
 }
 
 int		get_limit(char const *string, char const *set, int flag)
 {
 	unsigned int position;
-	unsigned int sum;
+	int sum;
 
 	if (flag == 1)
 	{
@@ -50,7 +52,7 @@ int		get_limit(char const *string, char const *set, int flag)
 		sum = -1;
 		position = ft_strlen((char*)string) - 1;
 	}
-	while (string[position] && ft_checkchar(string[position], set) && !position)
+	while (position && string[position] && ft_checkchar(string[position], set))
 		position += sum;
 	return (position);
 }
