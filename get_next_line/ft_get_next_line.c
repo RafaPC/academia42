39f0278746ea70e1789827fb00b4aecb467c85d0
Rafael Prieto ciprian <rprieto-@c2r8s5.42madrid.com>
2019-12-27 16:12:28 +0100

    Ahora va peor pero creo que está mejor hecho

--- get_next_line/ft_get_next_line.c
+++ get_next_line/ft_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rprieto- <rprieto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 11:45:02 by rprieto-          #+#    #+#             */
-/*   Updated: 2019/12/18 16:11:47 by rprieto-         ###   ########.fr       */
+/*   Updated: 2019/12/27 16:05:39 by rprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,48 +35,6 @@ void	static cosa(int fd, char *buffer[BUFFER_SIZE], char **line)
 		buffer[fd] = NULL;
 }
 
-int		ft_get_next_line(int fd, char **line)
-{
-	// char	*buffer[BUFFER_SIZE + 1];
-	// char		*pointer;
-	// int			bytes_readed;
-	// int			index;
-	// static char	*table[4096];
-
-	// if (buffer[fd] != NULL)
-	// {
-	// 	cosa(fd, buffer, line);
-	// }
-	// else
-	// {
-	// 	pointer = malloc(BUFFER_SIZE);
-	// 	if ((bytes_readed = read(fd, pointer, BUFFER_SIZE)) == -1)
-	// 		return (-1);
-	// 	if (bytes_readed == 0)
-	// 		return (0);
-	// 	if(!buffer[fd])
-	// 		buffer[fd] = ft_strdup(buffer);
-	// 	if ((index = ft_index_of(pointer, '\n')) != -1) //Si entra es porque hay un (como mínimo) un salto de línea en lo que ha leído
-	// 	{
-	// 		*line = (char*)malloc((index + 1) * sizeof(char));
-	// 		ft_memcpy(*line, pointer, index + 1);
-	// 		// buffer[fd] = (char*)malloc(ft_strlen(pointer) - index);
-	// 		// ft_memcpy(buffer[fd], &pointer[index + 1], ft_strlen(pointer) - index);
-	// 	}
-	// 	else
-	// 	{
-	// 		*line = (char*)malloc(BUFFER_SIZE);
-	// 		ft_memcpy(*line, pointer, BUFFER_SIZE);
-	// 		while()
-	// 		{
-	// 		}
-	// 	}
-
-	// 	free(pointer);
-	// }
-	return (1);
-}
-
 int ft_get_next_line2(int fd, char **line)
 {
 	char		buffer[BUFFER_SIZE + 1];
@@ -92,26 +50,30 @@ int ft_get_next_line2(int fd, char **line)
 	exists_line = 0;
 	if (cadena[fd] == NULL)
 		cadena[fd] = ft_strdup("");
-	
-	while (!exists_line)
-	{
-		if((bytes_readed = read(fd, buffer, BUFFER_SIZE)) > 0)			//Entra si ha leído algo
+	if (ft_index_of(cadena[fd], '\n') == -1)
+		while (!exists_line)
 		{
-			exists_line = (ft_index_of(buffer, '\n') != -1) ? 1 : 0;
-			aux = cadena[fd];
-			cadena[fd] = malloc(ft_strlen(cadena[fd]) + ft_strlen(buffer + 1));
-			cadena[fd] = ft_strjoin(aux, buffer);
-			free(aux);
+			if((bytes_readed = read(fd, buffer, BUFFER_SIZE)) > 0)			//Entra si ha leído algo
+			{
+				buffer[bytes_readed] = '\0';
+				exists_line = (ft_index_of(buffer, '\n') != -1) ? 1 : 0;
+				aux = cadena[fd];
+				cadena[fd] = malloc(ft_strlen(cadena[fd]) + ft_strlen(buffer));
+				cadena[fd] = ft_strjoin(aux, buffer);
+				free(aux);
+			}
+			else
+				return (bytes_readed);
 		}
-		else
-			return (bytes_readed);
-	}
+	if(*line != NULL)
+		free(*line);
 	*line = malloc(ft_index_of(cadena[fd], '\n') + 2);
 	bytes_readed = ft_index_of(cadena[fd], '\n');
-	ft_memcpy(*line, cadena[fd], bytes_readed);
+	ft_memcpy(*line, cadena[fd], bytes_readed + 1);
 	aux = cadena[fd];
+	int test = ft_strlen(cadena[fd]) - bytes_readed;
 	cadena[fd] = malloc(ft_strlen(cadena[fd]) - bytes_readed);
-	ft_memcpy(cadena[fd], &aux[bytes_readed + 1], ft_strlen(&aux[bytes_readed + 1]) - bytes_readed);
+	ft_memcpy(cadena[fd], &aux[bytes_readed + 1], ft_strlen(&aux[bytes_readed + 1]));
 	free(aux);
 	return(1);
 }
@@ -121,8 +83,12 @@ int		main(void)
 	char	**line;
 	int i = 1;
 	int fd = open("/Users/rprieto-/Documents/academia42/get_next_line/test.txt", O_RDONLY);
-	if (ft_get_next_line2(fd, line) == 1)
+	while(ft_get_next_line2(fd, line) == 1)
 		printf("%d-%s", i, *line);
+	// if (ft_get_next_line2(fd, line) == 1)
+	// 	printf("%d-%s", i, *line);
+	// if (ft_get_next_line2(fd, line) == 1)
+	// 	printf("%d-%s", i, *line);
 	//printf("%s", *line);
 	close(fd);
 	return (0);
