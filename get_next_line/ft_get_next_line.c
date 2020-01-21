3f9de9225991946867b302308ef2111e7289a04f
Rafael Prieto ciprian <rprieto-@c2r8s6.42madrid.com>
2020-01-21 15:44:49 +0100

    Funciona mejor pero el free hace lo que le sale de los cojones

--- get_next_line/ft_get_next_line.c
+++ get_next_line/ft_get_next_line.c
@@ -6,34 +6,34 @@
 /*   By: rprieto- <rprieto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 11:45:02 by rprieto-          #+#    #+#             */
-/*   Updated: 2019/12/27 16:05:39 by rprieto-         ###   ########.fr       */
+/*   Updated: 2020/01/21 15:40:27 by rprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "get_next_line.h"
 
-void	static cosa(int fd, char *buffer[BUFFER_SIZE], char **line)
-{
-	int		index;
-	char	*aux;
-	int		len;
+// void	static cosa(int fd, char *buffer[BUFFER_SIZE], char **line)
+// {
+// 	int		index;
+// 	char	*aux;
+// 	int		len;
 
-	if((index = ft_index_of(buffer[fd], '\n')) != -1)
-	{
-		free(*line);
-		*line = (char*)malloc((index + 2) * sizeof(char));
-		ft_memcpy(*line, buffer[fd], index + 1);
-		(*line)[index + 1] = '\0';
-		aux = buffer[fd];
-		len = ft_strlen(aux) - index;
-		buffer[fd] = (char*)malloc(len * sizeof(char));
-		ft_memcpy(buffer[fd], &aux[index + 1], len);
-		free(aux);
-	}
-	else
-		buffer[fd] = NULL;
-}
+// 	if((index = ft_index_of(buffer[fd], '\n')) != -1)
+// 	{
+// 		free(*line);
+// 		*line = (char*)malloc((index + 2) * sizeof(char));
+// 		ft_memcpy(*line, buffer[fd], index + 1);
+// 		(*line)[index + 1] = '\0';
+// 		aux = buffer[fd];
+// 		len = ft_strlen(aux) - index;
+// 		buffer[fd] = (char*)malloc(len * sizeof(char));
+// 		ft_memcpy(buffer[fd], &aux[index + 1], len);
+// 		free(aux);
+// 	}
+// 	else
+// 		buffer[fd] = NULL;
+// }
 
 int ft_get_next_line2(int fd, char **line)
 {
@@ -47,10 +47,13 @@ int ft_get_next_line2(int fd, char **line)
 	// {
 
 	// }
+	aux = 0;
 	exists_line = 0;
+	bytes_readed = 0;
 	if (cadena[fd] == NULL)
 		cadena[fd] = ft_strdup("");
-	if (ft_index_of(cadena[fd], '\n') == -1)
+	// if (ft_index_of(cadena[fd], '\n') == -1)
+		exists_line = (ft_index_of(cadena[fd], '\n') != -1) ? 1 : 0;
 		while (!exists_line)
 		{
 			if((bytes_readed = read(fd, buffer, BUFFER_SIZE)) > 0)			//Entra si ha leído algo
@@ -60,31 +63,38 @@ int ft_get_next_line2(int fd, char **line)
 				aux = cadena[fd];
 				cadena[fd] = malloc(ft_strlen(cadena[fd]) + ft_strlen(buffer));
 				cadena[fd] = ft_strjoin(aux, buffer);
+				aux = 0;
 				free(aux);
 			}
 			else
 				return (bytes_readed);
 		}
 	if(*line != NULL)
+	{	
 		free(*line);
+		*line = malloc(0);
+		*line = 0;
+	}
 	*line = malloc(ft_index_of(cadena[fd], '\n') + 2);
 	bytes_readed = ft_index_of(cadena[fd], '\n');
-	ft_memcpy(*line, cadena[fd], bytes_readed + 1);
+	*line = ft_memcpy(*line, cadena[fd], bytes_readed + 1);
 	aux = cadena[fd];
-	int test = ft_strlen(cadena[fd]) - bytes_readed;
+	// int test = ft_strlen(cadena[fd]) - bytes_readed;
 	cadena[fd] = malloc(ft_strlen(cadena[fd]) - bytes_readed);
-	ft_memcpy(cadena[fd], &aux[bytes_readed + 1], ft_strlen(&aux[bytes_readed + 1]));
+	cadena[fd] = ft_memcpy(cadena[fd], &aux[bytes_readed + 1], ft_strlen(&aux[bytes_readed + 1]));
+	aux = 0;
 	free(aux);
 	return(1);
 }
 
 int		main(void)
 {
-	char	**line;
+	char	*line;
+	line = 0;
 	int i = 1;
 	int fd = open("/Users/rprieto-/Documents/academia42/get_next_line/test.txt", O_RDONLY);
-	while(ft_get_next_line2(fd, line) == 1)
-		printf("%d-%s", i, *line);
+	while(ft_get_next_line2(fd, &line) == 1)
+		printf("%d-%s", i, line);
 	// if (ft_get_next_line2(fd, line) == 1)
 	// 	printf("%d-%s", i, *line);
 	// if (ft_get_next_line2(fd, line) == 1)
