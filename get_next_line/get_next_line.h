bb105c1e7ba9733ca029ac3dbdd06fe9eea8f3f1
Rafael Prieto ciprian <rprieto-@c1r6s4.42madrid.com>
2020-02-06 12:57:40 +0100

    Estructura añadida.

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,26 +6,32 @@
 /*   By: rprieto- <rprieto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 15:16:24 by rprieto-          #+#    #+#             */
-/*   Updated: 2020/01/29 15:41:24 by rprieto-         ###   ########.fr       */
+/*   Updated: 2020/02/06 12:35:04 by rprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+	# define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 2
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <stdio.h>
-# ifndef NULL
-# define NULL 0
-# endif
-
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strrchr(const char *s, int c);
-int		ft_get_index_of(char *string, char c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+	# include <unistd.h>
+	# include <stdlib.h>
+	# include <fcntl.h>
+	# include <stdio.h>
+	# ifndef NULL
+		# define NULL 0
+	# endif
+	#define BUFFER_SIZE 42
+	typedef struct	s_file
+	{
+		int				fd;
+		char			*buffer;
+		t_file			*next;
+	}				t_file;
+	int		get_next_line(int fd, char **line);
+	size_t	ft_strlen(char *s);
+	char	*ft_strdup(char *s1);
+	char	*ft_strjoin(char *s1, char *s2);
+	char	*ft_strrchr(char *s, int c);
+	int		ft_get_index_of(char *string, char c);
+	char	*ft_substr(char *s, unsigned int start, size_t len);
 #endif
