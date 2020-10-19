c2066947acda61fec9067c3fcbff4d8c30bde106
Rafael Prieto <rafitap.c@hotmail.com>
2020-10-19 21:55:40 +0200

    Cub3D main

---
+++ cub3D/main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rprieto- <rprieto-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/17 15:39:21 by rprieto-          #+#    #+#             */
+/*   Updated: 2020/10/19 20:09:29 by rprieto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+#include "libft/libft.h"
+
+int main(int argc, char const *argv[])
+{
+	t_error_info	*error_info;
+	t_bool			save_img;
+
+	save_img = false;
+	// if (argc == 3)
+	// {
+	// 	if (ft_strncmp(argv[1], "--save", 6))
+	// 		save_img = true;
+	// }
+	char	*file = (char*)argv[1];
+	check_map(error_info, file);
+	return 0;
+}
