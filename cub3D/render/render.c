3f081179eddf8731638db9048d2ee963f35e6fe3
Rafael Prieto <rafitap.c@hotmail.com>
2020-11-15 13:18:41 +0100

    Two ways of render the column

--- cub3D/render/render.c
+++ cub3D/render/render.c
@@ -6,7 +6,7 @@
 /*   By: rprieto- <rprieto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 17:38:49 by rprieto-          #+#    #+#             */
-/*   Updated: 2020/11/12 12:49:28 by rprieto-         ###   ########.fr       */
+/*   Updated: 2020/11/15 12:50:12 by rprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -140,9 +140,63 @@ void	draw_map(t_vars *vars)
 	}
 }
 
-
-
 void	render_column(t_vars *vars, float distance)
+{
+	int column_height;
+	int color;
+	int ceil_color;
+	int floor_color;
+	int screen_width;
+	int screen_height;
+
+	screen_width = 1920;
+	screen_height = 1080;
+	ceil_color = create_trgb(0, 102, 217, 255);
+	floor_color = create_trgb(0, 51, 153, 51);
+	//Calculate column height based on the distance to the wall
+	column_height = (int)(8 * 90)/distance;
+	//Check column limits
+	if (column_height > screen_height)
+		column_height = screen_height;
+	// else if (column_height < 30)
+	// 	column_height = 30;
+	//Dibuja techo y suelo
+	if (column_height < screen_height)
+	{
+		draw_square(1, (screen_height - column_height)/2, offset_column, 0, ceil_color, vars);
+		draw_square(1, (screen_height - column_height)/2, offset_column, (screen_height / 2) + (column_height / 2), floor_color, vars);
+		//Esto era para poner el suelo más "arriba" más oscuro
+		// for (int i = 300 + (column_height / 2); i < 600; i++)
+		// 	my_mlx_pixel_put(vars->img, offset_column, i, add_shade((float)(600 - i/700), floor_color));
+	}
+		//Forma mía
+		int drawStart = screen_height / 2;
+		int drawEnd = column_height / 2;
+		int color_text;
+		int diferencia = (8 * 90)/distance;
+		for (float i = 0; (int)i <= drawEnd; i++)
+		{
+			//-----------DEBUG
+			// double x = (i - drawStart) / column_height;
+			// if (!debug)
+			// {
+			// 	printf("i:%i\n", i);
+			// 	printf("column height:%i\n", column_height);
+			// 	printf("Valor de y(entre 0 y 1): %f\n", x);
+			// }
+			// color_text = get_pixel(texture.data, (int)(texture.width * vars->texture_x),
+			// (int)(texture.height * ((i - drawStart) / column_height)));
+			// column_height = (8 * 90)/distance;
+			color_text = get_image_colour(vars, column_height, i, diferencia);
+			my_mlx_pixel_put(vars->img, offset_column, drawStart + (int)i, color_text);
+			color_text = get_image_colour(vars, column_height, -i, diferencia);
+			my_mlx_pixel_put(vars->img, offset_column, drawStart - (int)i, color_text);
+		}
+		debug = 1;
+	offset_column += 1;
+}
+
+void	render_column_MIO(t_vars *vars, float distance)
 {
 	int column_height;
 	int color;
@@ -162,13 +216,6 @@ void	render_column(t_vars *vars, float distance)
 		column_height = screen_height;
 	else if (column_height < 30)
 		column_height = 30;
-	//Checkea que pared va a visualizar
-	if (vars->wall_face == north_face || vars->wall_face == south_face)
-		color = create_trgb(0, 100, 200, (vars->wall_face == north_face) ? 255 : 80);
-	else if (vars->wall_face == west_face || vars->wall_face == east_face)
-		color = create_trgb(0, 200, (vars->wall_face == east_face) ? 255 : 80, 100);
-	//Añade sombreado
-	color = add_shade(distance / 9, color);
 	//Dibuja techo y suelo
 	if (column_height < screen_height)
 	{
@@ -178,41 +225,19 @@ void	render_column(t_vars *vars, float distance)
 		// for (int i = 300 + (column_height / 2); i < 600; i++)
 		// 	my_mlx_pixel_put(vars->img, offset_column, i, add_shade((float)(600 - i/700), floor_color));
 	}
-		// int color;
-		// for (int j = 0; j < column_height/2; j++)
-		// {
-		// 	color = get_image_colour(vars, column_height, j);
-		// 	my_mlx_pixel_put(vars->img, offset_column, screen_height/2 + j, color);
-		// 	color = get_image_colour(vars, column_height, -j);
-		// 	my_mlx_pixel_put(vars->img, offset_column, screen_height/2 - j, color);
-		// }
-		//Forma mía
-		int drawStart = screen_height / 2 - column_height / 2;
-		int drawEnd = drawStart + column_height;
-		int color_text;
-		for (float i = drawStart; (int)i <= drawEnd; i++)
-		{
-			//-----------DEBUG
-			// double x = (i - drawStart) / column_height;
-			// if (!debug)
-			// {
-			// 	printf("i:%i\n", i);
-			// 	printf("column height:%i\n", column_height);
-			// 	printf("Valor de y(entre 0 y 1): %f\n", x);
-			// }
-			// color_text = get_pixel(texture.data, (int)(texture.width * vars->texture_x),
-			// (int)(texture.height * ((i - drawStart) / column_height)));
-			// column_height = (8 * 90)/distance;
-			int diferencia = (8 * 90)/distance - column_height;
-			color_text = get_image_colour_MIO(vars, column_height, (i - drawStart) / (column_height), diferencia);
-			// color_text = get_image_colour_MIO(vars, column_height, (i - drawStart) / column_height);
-			// color_text = create_trgb(0, 200,  150, 100);
-			my_mlx_pixel_put(vars->img, offset_column, (int)i, color_text);
-		}
-		debug = 1;
+	//Forma mía
+	int drawStart = screen_height / 2 - column_height / 2;
+	int drawEnd = drawStart + column_height;
+	int color_text;
+	for (float i = drawStart; (int)i <= drawEnd; i++)
+	{
+		color_text = create_trgb(0, 255, 70, 70);
+		color_text = get_image_colour_MIO(vars, (i - drawStart)/column_height);
+		my_mlx_pixel_put(vars->img, offset_column, (int)i, color_text);
+	}
+	debug = 1;
 	offset_column += 1;
 }
-
 unsigned int    get_pixel(t_data *image, int x, int y)
 {
     unsigned int	*dst;
